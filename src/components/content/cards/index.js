bf2d54cf8bce51d7c613864e55191115dadb71b3
SkynImIk <vovkuz10.com>
2022-11-24 00:24:18 +0200

    fd

--- src/components/content/cards/index.js
+++ src/components/content/cards/index.js
@@ -1,10 +1,41 @@
 import React from "react";
 import './style.css';
+import 
 
-function Card () {
+function Card (props) {
     return(
-        <div className="all">
+    const classNames = [
+    'card'
+   ];
+   
+   // if (props.isVisible == false) {
+   if (!props.isVisible) {
+    classNames.push('hide');
+   }
+   return (
+    <div className={classNames.join(' ')}>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-image"
+    style={{
+     backgroundImage: `url(${props.image})`
+    }}></div>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-title"
+   >{props.title}</div>
+   <div className="card-description">{props.description}</div>
+   <div className="card-date-section">
+    {props.date} |
+    <span
+     onClick={() => props.setTag(props.tag)}
+    >
+     {props.tag}
+    </span>
 
+
+   </div>
+  </div>
         </div>
     )
 }
